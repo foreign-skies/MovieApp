254b0d2eec8e830da45efbf8bc730035bfa04fa7
foreign-skies <56491559+foreign-skies@users.noreply.github.com>
2021-11-17 16:40:35 -0700

    Completed Add Service UI

--- app/src/main/java/com/example/movieapp/ServicesFragment.java
+++ app/src/main/java/com/example/movieapp/ServicesFragment.java
@@ -4,11 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 public class ServicesFragment extends Fragment {
+    ImageView netflixImage;
+    ImageView huluImage;
+    Button add_service;
+    int counter = 1;
     public static ServicesFragment newInstance() {
         ServicesFragment fragment = new ServicesFragment();
 
@@ -20,6 +26,22 @@ public class ServicesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.services_fragment, container, false);
+        netflixImage = view.findViewById(R.id.imageNetflix);
+        netflixImage.setVisibility(View.INVISIBLE);
+        huluImage = view.findViewById(R.id.imageHulu);
+        huluImage.setVisibility(View.INVISIBLE);
+        add_service = view.findViewById(R.id.AddServiceButton);
+        add_service.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (counter == 1)
+                    netflixImage.setVisibility(View.VISIBLE);
+                if (counter == 2)
+                    huluImage.setVisibility(View.VISIBLE);
+                counter++;
+            }
+        });
+
 
         return view;
     }
